709a4cc923e690a2d6ad18ead9687964ed22cfea
KimSungJun <arduous1130@gmail.com>
2018-02-05 22:38:51 +0900

    [2018-02-05] 141p까지함

--- MyMemory/MemoFormVC.swift
+++ MyMemory/MemoFormVC.swift
@@ -8,20 +8,63 @@
 
 import UIKit
 
-class MemoFormVC: UIViewController {
+class MemoFormVC: UIViewController,
+                    UIImagePickerControllerDelegate,
+                    UINavigationControllerDelegate,
+                    UITextViewDelegate{
+    
+    var subject: String!
+    
+    @IBOutlet weak var contents: UITextView!
+    
+    @IBOutlet weak var preview: UIImageView!
 
+    // 저장 버튼을 클릭했을 때 호출되는 메소드
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    // 카메라 버튼을 클릭했을 때 호출되는 메소드
+    @IBAction func pick(_ sender: Any) {
+        // 이미지 피커 인슨턴스를 생성한다.
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        // 이미지 피커 화면을 표시한다.
+        self.present(picker, animated: false)
+    }
+    
+    // 이미지 선택을 완료했을 때 호출되는 메소드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // 선택된 이미지를 미리보기에 표시한다.
+        self.preview.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        
+        // 이미지 피커 컨트롤러를 닫는다.
+        picker.dismiss(animated: false)
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.contents.delegate = self
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        // 내용의 최대 15자리까지 읽어 subject 변수에 저장한다.
+        let contents = textView.text as NSString
+        let length = ( (contents.length > 15) ? 15 : contents.length )
+        self.subject = contents.substring(with: NSRange(location: 0, length: length))
+        
+        // 내비게이션 타이틀에 표시한다.
+        self.navigationItem.title = subject
     }
 
+    /*
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
+    */
+ 
     /*
     // MARK: - Navigation
 
@@ -31,5 +74,4 @@ class MemoFormVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
