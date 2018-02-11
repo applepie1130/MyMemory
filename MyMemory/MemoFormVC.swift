12b6f116eb3e135b3c779015f2e47b40ccf8e27c
KimSungJun <arduous1130@gmail.com>
2018-02-11 23:32:27 +0900

    [2018-02-11] 146p까지함

--- MyMemory/MemoFormVC.swift
+++ MyMemory/MemoFormVC.swift
@@ -21,7 +21,33 @@ class MemoFormVC: UIViewController,
 
     // 저장 버튼을 클릭했을 때 호출되는 메소드
     @IBAction func save(_ sender: Any) {
+        // 내용을 입력하지 않았을 경우, 경고한다.
+        guard self.contents.text?.isEmpty == false else {
+            let alert = UIAlertController(title: nil,
+                                          message: "내용을 입력해주세요",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        // MemoData 객체를 생성하고, 데이터를 담는다.
+        let data = MemoData()
+        
+        data.title = self.subject // 제목
+        data.contents = self.contents.text // 내용
+        data.image = self.preview.image // 이미지
+        data.regdate = Date() // 작성시각
+        
+        // 앱 델리게이트 객체를 읽어온 다음, memolist 배열에 MemoData 객체를 추가한다.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.memoList.append(data)
+        
+        // 작성폼 화면을 종료하고, 이전 화면으로 돌아간다.
+        _ = self.navigationController?.popViewController(animated: true)        
     }
+    
+    
     
     // 카메라 버튼을 클릭했을 때 호출되는 메소드
     @IBAction func pick(_ sender: Any) {
