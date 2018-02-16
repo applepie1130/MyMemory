449894394b1a31a056f0e3c0bb469c51713e344f
KimSungJun <arduous1130@gmail.com>
2018-02-16 13:04:01 +0900

    [2018-02-16] 160p까지함

--- MyMemory/MemoFormVC.swift
+++ MyMemory/MemoFormVC.swift
@@ -10,8 +10,7 @@ import UIKit
 
 class MemoFormVC: UIViewController,
                     UIImagePickerControllerDelegate,
-                    UINavigationControllerDelegate,
-                    UITextViewDelegate{
+                    UINavigationControllerDelegate {
     
     var subject: String!
     
@@ -30,21 +29,21 @@ class MemoFormVC: UIViewController,
             self.present(alert, animated: true)
             return
         }
-        
+
         // MemoData 객체를 생성하고, 데이터를 담는다.
         let data = MemoData()
-        
+
         data.title = self.subject // 제목
         data.contents = self.contents.text // 내용
         data.image = self.preview.image // 이미지
         data.regdate = Date() // 작성시각
-        
+
         // 앱 델리게이트 객체를 읽어온 다음, memolist 배열에 MemoData 객체를 추가한다.
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         appDelegate.memoList.append(data)
-        
+
         // 작성폼 화면을 종료하고, 이전 화면으로 돌아간다.
-        _ = self.navigationController?.popViewController(animated: true)        
+        _ = self.navigationController?.popViewController(animated: true)
     }
     
     
@@ -53,10 +52,10 @@ class MemoFormVC: UIViewController,
     @IBAction func pick(_ sender: Any) {
         // 이미지 피커 인슨턴스를 생성한다.
         let picker = UIImagePickerController()
-        
+
         picker.delegate = self
         picker.allowsEditing = true
-        
+
         // 이미지 피커 화면을 표시한다.
         self.present(picker, animated: false)
     }
@@ -65,39 +64,19 @@ class MemoFormVC: UIViewController,
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         // 선택된 이미지를 미리보기에 표시한다.
         self.preview.image = info[UIImagePickerControllerEditedImage] as? UIImage
-        
+
         // 이미지 피커 컨트롤러를 닫는다.
         picker.dismiss(animated: false)
     }
     
-    override func viewDidLoad() {
-        self.contents.delegate = self
-    }
     
     func textViewDidChange(_ textView: UITextView) {
         // 내용의 최대 15자리까지 읽어 subject 변수에 저장한다.
         let contents = textView.text as NSString
         let length = ( (contents.length > 15) ? 15 : contents.length )
         self.subject = contents.substring(with: NSRange(location: 0, length: length))
-        
+
         // 내비게이션 타이틀에 표시한다.
         self.navigationItem.title = subject
     }
-
-    /*
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    */
- 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
